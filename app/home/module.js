95f0cc5d7a05341338001f0430db07ce41d21c18
itcast <test@itcast.cn>
2016-12-13 09:06:44 +0800

    12-12完结

--- app/home/module.js
+++ app/home/module.js
@@ -2,12 +2,14 @@
  * @Author: Administrator
  * @Date:   2016-12-12 16:54:55
  * @Last Modified by:   Administrator
- * @Last Modified time: 2016-12-12 19:39:05
+ * @Last Modified time: 2016-12-12 20:57:02
  */
 (function(angular) {
     angular.module('moviecat-home', ['ngRoute'])
         .config(['$routeProvider', function($routeProvider) {
-            $routeProvider.when('/home_page', {
+            $routeProvider.when('/home_page?', {
+                templateUrl: './home/homeView.html'
+            }).when('/?', {
                 templateUrl: './home/homeView.html'
             });
         }]);
